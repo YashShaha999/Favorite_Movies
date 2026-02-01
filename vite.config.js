c85fe435a58e4a1dcca9bdab48e91848b3dfaf65
Yash Shaha <yashshaha999@gmail.com>
2026-02-01 15:59:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Favorites_Movies", // make sure to include the trailing slash
+  base: "/Movies", // make sure to include the trailing slash
 });
